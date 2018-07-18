fddca68117f7e5ca2c2c8869fc5a5a451bac6534
Juan <juanmataruiz84@gmail.com>
2018-07-18 15:50:27 +0100

    game can check for a winner

--- tictactoe/src/components/GameInfo.js
+++ tictactoe/src/components/GameInfo.js
@@ -3,8 +3,12 @@ import React from 'react';
 class GameInfo extends React.Component {
 
   render() {
+    let element = <p>Current Player: {this.props.currentPlayer}</p>
+    if (this.props.winner) element = <p>The winner is: {this.props.winner}</p>
     return (
-      <p>Current Player: {this.props.currentPlayer}</p>
+      <div>
+        {element}
+      </div>
     )
   }
 }
--- tictactoe/src/containers/GameContainer.js
+++ tictactoe/src/containers/GameContainer.js
@@ -12,7 +12,8 @@ class GameContainer extends React.Component {
         ["", "", ""],
         ["", "", ""]
       ],
-      currentPlayer: 'X'
+      currentPlayer: 'X',
+      winner: null
     }
     this.handleClick = this.handleClick.bind(this)
     this.handlReset = this.handlReset.bind(this)
@@ -21,7 +22,7 @@ class GameContainer extends React.Component {
     return (
       <div className='game-container'>
         <Board handleClick={this.handleClick} boardState={this.state.boardState} />
-        <GameInfo currentPlayer={this.state.currentPlayer} />
+        <GameInfo currentPlayer={this.state.currentPlayer} winner={this.state.winner} />
         <Reset handlReset={this.handlReset} />
       </div>
     )
@@ -34,16 +35,44 @@ class GameContainer extends React.Component {
     const col = index % 3
 
     newGameState[row][col] = this.state.currentPlayer
+    let currentPlayer = this.state.currentPlayer === 'X' ? 'O' : 'X'
+    const winner = this.checkWinner()
+    console.log(winner);
+
+    if (winner) currentPlayer = ''
+
     this.setState({
       boardState: newGameState,
-      currentPlayer: this.state.currentPlayer === 'X' ? 'O' : 'X'
+      currentPlayer: currentPlayer,
+      winner: winner
     })
+  }
+
+  checkWinner() {
+    //check rows
+    for (let row of this.state.boardState) {
+      if (row[0] != '' && row[0] === row[1] && row[1] === row[2]) return row[0]
+    }
+
+    const state = this.state.boardState
+    //check columns
+    for (let i = 0; i < 3; i++) {
+
+      if (state[0][i] != '' && state[0][i] === state[1][i] && state[2][i]) return state[0][i];
+    }
+
+    //check diagonals
+    if (state[0][0] != '' && state[0][0] === state[1][1] && state[1][1] === state[2][2]) return state[0][0]
+    if (state[2][0] != '' && state[2][0] === state[1][1] && state[1][1] === state[0][2]) return state[2][0]
+
+
+    return null;
+
   }
 
   handlReset(event) {
     const newGameState = this.state.boardState.map((row) => row.map((element) => ""))
-    this.setState({ boardState: newGameState, currentPlayer: "X" })
-
+    this.setState({ boardState: newGameState, currentPlayer: "X", winner: null })
   }
 
 }
